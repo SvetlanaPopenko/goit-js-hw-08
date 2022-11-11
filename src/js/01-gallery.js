0d3d1f4d8cc8dd0242199b7b2e422ced215a540e
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-11 03:21:02 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
+
+const containerGallery = document.querySelector('.gallery');
+
+function addGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<a class = "gallery__item" href = "${original}">
+    <img class = "gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+    )
+    .join('');
+}
+
+containerGallery.insertAdjacentHTML('beforeend', addGallery(galleryItems));
 console.log(galleryItems);
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
