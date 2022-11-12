f2df7cd66af7eb427e1d53f4f46e6761e50f9b41
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-12 16:49:55 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,16 @@
-import player from '@vimeo/player';
-console.log(player);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+if (localStorage.getItem(currentTime)) {
+  player.setCurrentTime(localStorage.getItem(currentTime));
+}
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.setItem(currentTime, JSON.stringify(data.seconds));
+}
